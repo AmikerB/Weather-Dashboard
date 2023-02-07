250ba8983f473001f252d4b9a9d6ab0dfe82f1af
AmikerB <118816092+AmikerB@users.noreply.github.com>
2023-02-07 20:19:03 +0000

    comments added and credits

--- assets/js/history.js
+++ assets/js/history.js
@@ -1,5 +1,3 @@
-// display history seaches as buttons
-// limit to 6 history searches
 
 const historyBtns = $('#history');
 
@@ -8,9 +6,9 @@ let storedCities = localStorage.getItem("cityHistory");
 
 function getHistory() {
     historyBtns.empty();
-
+    // limit to 6 history searches
     history = history.slice(0, 6);
-
+    // display history searches as buttons
     history.forEach(function (city) {
         const btn = $("<button>").text(city);
         btn.addClass("history-button");
@@ -19,8 +17,11 @@ function getHistory() {
     });
 }
 
+// if not equal to null 
 if (storedCities !== null) {
+    // parse JSON string stored in stored cities and assign to history
     history = JSON.parse(storedCities);
+    // convert history into an array and removes duplicate values
     history = Array.from(new Set(history));
     getHistory();
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,118 +1,117 @@
-function weather(section, response, sectionTitle) {
-    const weatherIcon = response.weather[0].icon;
-    const todaysWeatherIcon = `https://openweathermap.org/img/wn/${weatherIcon}.png`;
-    const createImgTag = $("<img>");
-    createImgTag.attr("src", todaysWeatherIcon);
-    createImgTag.attr("alt", "weather icon");
+$(document).ready(function () {
 
-    const todaysTemp = response.main.temp;
-    const todaysTempSection = $("<h4>").text(`Temp: ${todaysTemp}°C`);
+    function weather(section, response, sectionTitle) {
+        // weather icons
+        const weatherIcon = response.weather[0].icon;
+        const todaysWeatherIcon = `https://openweathermap.org/img/wn/${weatherIcon}.png`;
+        const createImgTag = $("<img>");
+        createImgTag.attr("src", todaysWeatherIcon);
+        createImgTag.attr("alt", "weather icon");
+        // temp
+        const todaysTemp = response.main.temp;
+        const todaysTempSection = $("<h4>").text(`Temp: ${todaysTemp}°C`);
+        // wind
+        const todaysWind = response.wind.speed;
+        const todaysWindSection = $("<h4>").text(`Wind: ${todaysWind} mph`);
+        // humidity
+        const todaysHumidity = response.main.humidity;
+        const todaysHumiditySection = $("<h4>").text(`Humidity: ${todaysHumidity}%`);
+        // assign to section
+        section.empty().append(sectionTitle, createImgTag, todaysTempSection, todaysWindSection, todaysHumiditySection);
+    }
 
-    const todaysWind = response.wind.speed;
-    const todaysWindSection = $("<h4>").text(`Wind: ${todaysWind} KPH`);
+    // function for todays forcast
+    function todaysForecast(citySearch) {
 
-    const todaysHumidity = response.main.humidity;
-    const todaysHumiditySection = $("<h4>").text(`Humidity: ${todaysHumidity}%`);
+        const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&mode=json&units=metric&appid=647cd0964ccafc21b61d37e25926911b`;
 
-    section.empty().append(sectionTitle, createImgTag, todaysTempSection, todaysWindSection, todaysHumiditySection);
-}
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
 
-// function for todays forcast
-function todaysForecast(citySearch) {
+            const cityName = response.name;
+            const todaysDate = moment().format('DD/MM/YYYY');
+            const cityTitle = $("<h2>").text(`${cityName} ${todaysDate}`)
 
-    const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&mode=json&units=metric&appid=647cd0964ccafc21b61d37e25926911b`;
+            const todaySection = $('#today');
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        const cityName = response.name;
-        const todaysDate = moment().format('DD/MM/YYYY');
-        const cityTitle = $("<h2>").text(`${cityName} ${todaysDate}`)
+            todaySection.addClass('todaySection');
 
-        const todaySection = $('#today');
+            weather(todaySection, response, cityTitle);
+            // only save to local storage if city isn't already there 
+            if (!history.includes(cityName)) {
+                // adds city to index 0 of history array 
+                history.unshift(cityName);
+                // save to local storage
+                localStorage.setItem("cityHistory", JSON.stringify(history));
 
-        todaySection.addClass('todaySection');
-
-        weather(todaySection, response, cityTitle);
-        // only save to local storage if city isn't already there 
-        if (!history.includes(cityName)) {
-            // adds city to index 0 of history array 
-            history.unshift(cityName);
-            // save to local storage
-            localStorage.setItem("cityHistory", JSON.stringify(history));
-
-            getHistory();
-        }
-
-    }).catch(function (error) {
-        console.error(error);
-        alert("No results found for the given city");
-        // clears input
-        $('#search-input').val("");
-    });
-}
-
-// function for 5 day forecast 
-function fiveDayForecast(citySearch) {
-    const forecastHeader = $('.forecast-title')
-
-    forecastHeader.empty();
-    const forecastTitle = '5 Day Forecast:';
-    forecastHeader.append(forecastTitle);
-
-    const queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + citySearch + '&units=metric&appid=647cd0964ccafc21b61d37e25926911b&cnt=40';
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        const forecastSection = $('#forecast');
-        // selecting to display the weather at 12 noon 
-        const middayList = response.list.filter(obj => obj.dt_txt.endsWith("12:00:00"));
-
-        forecastSection.empty();
-
-        middayList.forEach(obj => {
-            // creating new div for each and styling them
-            const newDiv = $("<div>").text(obj)
-
-            newDiv.addClass('forecastDivs');
-
-            forecastSection.append(newDiv);
-
-            // day of the week
-            const date = obj.dt_txt.split(" ");
-            // selecting only the date
-            const dateOnly = date[0];
-            // converting to day of the week
-            const dayOfWeek = moment(dateOnly).format('dddd');
-            // title of the new div
-            const title = $("<h3>").text(`${dayOfWeek}`);
-
-            // passing into weather funcition to display each days weather
-            weather(newDiv, obj, title);
+                getHistory();
+            }
+            // if error comes back
+        }).catch(function (error) {
+            console.error(error);
+            alert("No results found for this city");
+            // clears input
+            $('#search-input').val("");
         });
+    }
+
+    // function for 5 day forecast 
+    function fiveDayForecast(citySearch) {
+
+        const forecastHeader = $('.forecast-title')
+
+        const queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${citySearch}&units=metric&appid=647cd0964ccafc21b61d37e25926911b&cnt=40`;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            forecastHeader.empty();
+            const forecastTitle = '5 Day Forecast:';
+            forecastHeader.append(forecastTitle);
+
+            const forecastSection = $('#forecast');
+            // selecting to display the weather at 12 noon 
+            const middayList = response.list.filter(obj => obj.dt_txt.endsWith("12:00:00"));
+
+            forecastSection.empty();
+
+            middayList.forEach(obj => {
+                // creating new div for each and styling them
+                const newDiv = $("<div>").text(obj)
+                newDiv.addClass('forecastDivs');
+                forecastSection.append(newDiv);
+                // day of the week
+                const date = obj.dt_txt.split(" ");
+                // selecting only the date
+                const dateOnly = date[0];
+                // converting to day of the week
+                const dayOfWeek = moment(dateOnly).format('dddd');
+                // title of the new div
+                const title = $("<h3>").text(`${dayOfWeek}`);
+
+                // passing into weather funcition to display each days weather
+                weather(newDiv, obj, title);
+            });
+        })
+    }
+
+    $('#search-form').on('submit', function (event) {
+        event.preventDefault();
+        const cityInput = $('#search-input').val();
+        todaysForecast(cityInput);
+        fiveDayForecast(cityInput);
+
+    });
+
+    historyBtns.on('click', $('button'), function (event) {
+        event.preventDefault();
+        const cityBtn = event.target.textContent;
+        todaysForecast(cityBtn);
+        fiveDayForecast(cityBtn);
     })
-}
 
-$('#search-form').on('submit', function (event) {
-
-    event.preventDefault();
-
-    const cityInput = $('#search-input').val();
-
-    todaysForecast(cityInput);
-
-    fiveDayForecast(cityInput);
-
-});
-
-
-historyBtns.on('click', $('button'), function (event) {
-    event.preventDefault();
-    const cityBtn = event.target.textContent;
-    todaysForecast(cityBtn);
-    fiveDayForecast(cityBtn);
 })
-
